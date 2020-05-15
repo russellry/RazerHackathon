ba0d7a11c3df4dcfd41ae707c84b50efea873bad
russellry <digivicer@hotmail.com>
2020-05-15 12:44:37 +0800

    Initial Commit

---
+++ RazerHackathon/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RazerHackathon
+//
+//  Created by Russell Ong on 15/5/20.
+//  Copyright © 2020 trillion.unicorn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RazerHackathon/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RazerHackathon
+//
+//  Created by Russell Ong on 15/5/20.
+//  Copyright © 2020 trillion.unicorn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
